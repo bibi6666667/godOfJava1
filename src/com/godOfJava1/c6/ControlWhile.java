ef5de3fc58e9c94e754bd919400027b193ac8672
bibi6666667 <bibi6666667@kakao.com>
2020-11-18 22:10:14 +0900

    [6장] 반복문 - while, break, continue, 무한루프
    p,145-149 반복문 예제 실습

---
+++ src/com/godOfJava1/c6/ControlWhile.java
@@ -0,0 +1,57 @@
+package com.godOfJava1.c6;
+
+public class ControlWhile {
+    public static void main(String[] args) {
+        ControlWhile control = new ControlWhile();
+
+    }
+
+    public void whileLoop1() {
+        ControlOfFlow control = new ControlOfFlow();
+        int loop = 0;
+        while (loop < 12) {
+            loop++;
+            control.switchCalendar(loop);
+        }
+    }
+
+    public void whileLoop2() {
+        ControlOfFlow control = new ControlOfFlow();
+        int loop = 0;
+        while (loop < 12) {
+            loop++;
+            control.switchCalendar(loop);
+            if (loop == 6) loop = 100;
+        }
+    }
+
+    public void whileBreak(){
+        ControlOfFlow control = new ControlOfFlow();
+        int loop = 0;
+        while (loop < 12) {
+            loop++;
+            control.switchCalendar(loop);
+            if (loop == 6) break;
+        }
+    }
+
+    public void whileContinue() {
+        ControlOfFlow control = new ControlOfFlow();
+        int loop = 0;
+        while (loop < 12) {
+            loop++;
+            if (loop == 6) continue;
+            control.switchCalendar(loop);
+        }
+    }
+
+    public void whileContinueInfinit(){
+        ControlOfFlow control = new ControlOfFlow();
+        int loop = 0;
+        while (loop < 12) {
+            if (loop == 6) continue;
+            loop++;
+            control.switchCalendar(loop);
+        }
+    }
+}
