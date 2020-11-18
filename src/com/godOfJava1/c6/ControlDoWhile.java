a6aafad174383c88b1c7025b5162ba5479e24024
bibi6666667 <bibi6666667@kakao.com>
2020-11-18 22:10:56 +0900

    [6장] 반복문 - do-while 문
    p.150 do-while 문 예제 실습

---
+++ src/com/godOfJava1/c6/ControlDoWhile.java
@@ -0,0 +1,18 @@
+package com.godOfJava1.c6;
+
+
+public class ControlDoWhile {
+    public static void main(String[] args) {
+        ControlDoWhile control = new ControlDoWhile();
+        control.doWhile();
+    }
+
+    public void doWhile() {
+        ControlOfFlow control = new ControlOfFlow();
+        int loop = 0;
+        do {
+            loop ++;
+            control.switchCalendar(loop);
+        } while (loop < 12);
+    }
+}
