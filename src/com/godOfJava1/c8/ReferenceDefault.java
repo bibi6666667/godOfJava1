d0add663e390bc5f24f44b83b27bae94ebf464f7
bibi6666667 <bibi6666667@kakao.com>
2020-11-20 17:40:40 +0900

    [8장] 기본 생성자
    189 ) 기본 생성자는 아무런 매개변수가 없는 생성자이며, 선언하지 않아도 컴파일할 때 기본으로 만들어진다.
    (생성자명은 기본적으로 클래스명과 같다. 클래스의 객체를 생성하는 것이 생성자이기 때문)

---
+++ src/com/godOfJava1/c8/ReferenceDefault.java
@@ -0,0 +1,6 @@
+package com.godOfJava1.c8;
+
+public class ReferenceDefault {
+    ReferenceDefault reference = new ReferenceDefault(); // ReferenceDefault()가 기본 생성자이다.
+    // -> 아무런 매개변수가 없으며, 클래스 명과 동일한 이름에 뒤에()만 붙어있음.
+}
