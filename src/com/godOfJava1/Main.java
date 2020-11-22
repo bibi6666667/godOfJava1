adfdae8f2b6b5f6db69d960a864c3cde1b80002d
bibi6666667 <bibi6666667@kakao.com>
2020-11-22 23:43:04 +0900

    [9장] import로 다른 패키지 클래스에 접근하기
    226-228)
    import 예제 실습

--- src/com/godOfJava1/Main.java
+++ src/com/godOfJava1/Main.java
@@ -1,8 +1,21 @@
 package com.godOfJava1;
 
+import com.godOfJava1.c9.Sub;
+import com.godOfJava1.c9.SubStatic;
+// import 란.. 다른 패키지에 있는 클래스에 접근하기 위해 사용하는 예약어
+// import 패키지이름 클래스이름; 으로 선언하면 컴파일러가 그 패키지의 그 클래스를 읽어와 사용한다.
+
+import static com.godOfJava1.c9.SubStatic.subStaticMethod;
+import static com.godOfJava1.c9.SubStatic.CLASS_NAME;
+// import static으로 다른 패키지의 static변수들을 선언하면 객체를 불러오지 않고도 바로 사용 가능하다.
+/*또는*/import static com.godOfJava1.c9.SubStatic.*; //처럼 한 줄로도 선언 가능.
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Sub sub = new Sub();
+        sub.subClassMethod();
+        /*SubStatic.*/subStaticMethod();
+        System.out.println(/*SubStatic.*/CLASS_NAME);
     }
 }
---
+++ src/com/godOfJava1/c9/Sub.java
@@ -0,0 +1,10 @@
+package com.godOfJava1.c9;
+
+public class Sub {
+    public Sub(){
+
+    }
+    public void subClassMethod(){
+
+    }
+}
