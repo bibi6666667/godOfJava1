c1fc3f56d54b8368b21bc95de4d204336406cdb0
bibi6666667 <bibi6666667@kakao.com>
2020-11-22 23:47:41 +0900

    [9장] 클래스 접근제어자 유의점 & 하나의 클래스 소스에 여러 클래스 선언
    234-235)
    - public클래스는 하나만 선언 가능하며, 선언한 클래스 접근제어자는 클래스파일명과 같아야 한다.
    -하나의 클래스파일에 여러 클래스를 선언해도 괜찮다.

---
+++ src/com/godOfJava1/c9/PublicClass.java
@@ -0,0 +1,13 @@
+package com.godOfJava1.c9;
+
+public class PublicClass {
+    public static void main(String[] args) {
+
+    }
+}
+// 하나의 클래스 소스 파일에 여러 개의 클래스를 선언해도 괜찮다,
+// 단, public인 클래스 이름 = 클래스 소스 파일 이름 이어야 한다.
+// (다른 클래스에는 public이 붙어선 안 된다)
+/*public*/class PublicSecondClass {
+
+}
