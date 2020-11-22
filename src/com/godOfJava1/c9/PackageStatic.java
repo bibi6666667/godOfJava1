84eed5b36f0cf67f79637c2b38a2234423a7274f
bibi6666667 <bibi6666667@kakao.com>
2020-11-22 23:44:03 +0900

    [9장] import static
    228-229)
    import static 예제 실습

---
+++ src/com/godOfJava1/c9/PackageStatic.java
@@ -0,0 +1,8 @@
+package com.godOfJava1.c9;
+
+public class PackageStatic {
+    public static void main(String[] args) {
+        SubStatic.subStaticMethod();
+
+    }
+}
---
+++ src/com/godOfJava1/c9/SubStatic.java
@@ -0,0 +1,9 @@
+package com.godOfJava1.c9;
+
+public class SubStatic {
+    public final static String CLASS_NAME = "SubStatic"; // 클래스 변수(static 변수)
+
+    public static void subStaticMethod() { // static 메소드
+        System.out.println("subStaticMethod() is called.");
+    }
+}
