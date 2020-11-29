6def2339a8c34a1535d9dbf844578dc49088c599
bibi6666667 <bibi6666667@kakao.com>
2020-11-29 22:29:44 +0900

    [10장] 메소드 오버라이딩 - 접근제어자
    250-251
    자식 메소드는
    -오버라이딩한 메소드의 리턴타입을 마음대로 바꿀 수 없다.
    -오버라이딩한 메소드의 접근제어자는 확장되는 경우만 허용된다. (축소되는 경우는 컴파일에러)

---
+++ src/com/godOfJava1/c10inheritance/ChildOverridingPrivate.java
@@ -0,0 +1,10 @@
+package com.godOfJava1.c10inheritance;
+
+public class ChildOverridingPrivate extends ParentOverridingPrivate{
+    public ChildOverridingPrivate(){
+        System.out.println("ChildOverridingPrivate Constructor");
+    }
+    public void printName() {
+        System.out.println("ChildOverridingPrivate printName()");
+    }
+}
