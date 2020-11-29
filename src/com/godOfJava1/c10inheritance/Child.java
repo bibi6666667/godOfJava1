45d7170545d1dc3302be21240f10044ac997b851
bibi6666667 <bibi6666667@kakao.com>
2020-11-29 22:16:25 +0900

    [10장] 상속의 개념
    240-201 상속의 개념 예제 실습

---
+++ src/com/godOfJava1/c10inheritance/Child.java
@@ -0,0 +1,7 @@
+package com.godOfJava1.c10inheritance;
+
+public class Child extends Parent { // 부모 클래스 상속
+    public Child() {
+        System.out.println("Child Constructor");
+    }
+}
---
+++ src/com/godOfJava1/c10inheritance/Parent.java
@@ -0,0 +1,10 @@
+package com.godOfJava1.c10inheritance;
+
+public class Parent {
+    public Parent() {
+        System.out.println("Parent Constructor");
+    }
+    public void printName() {
+        System.out.println("Parent printName()");
+    }
+}
