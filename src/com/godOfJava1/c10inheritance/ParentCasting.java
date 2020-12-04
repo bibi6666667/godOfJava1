23775ba694650908bb5d28ed8934bdefcea48d5d
bibi6666667 <bibi6666667@kakao.com>
2020-12-05 01:45:00 +0900

    [10장] 형변환 - 학습 중..

---
+++ src/com/godOfJava1/c10inheritance/ParentCasting.java
@@ -0,0 +1,11 @@
+package com.godOfJava1.c10inheritance;
+
+public class ParentCasting {
+    public ParentCasting(){
+    }
+    public ParentCasting(String name){
+    }
+    public void printName(){
+        System.out.println("printName() - Parent");
+    }
+}
