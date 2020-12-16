1bab22fa8ade833a8667cd76a8caa8157acf444f
bibi6666667 <bibi6666667@kakao.com>
2020-12-17 00:47:51 +0900

    [13장] 인터페이스의 사용
    317
    
    인터페이스를 사용하려면 인터페이스 객체를 생성한 후, 인터페이스에 선언된 모든 메소드를 구현해 둔 클래스를 가져와 사용해야 한다.
    인터페이스의 메소드들을 구현해놓지 않고 인터페이스를 사용하려 하면, 아무것도 초기화되어있지 않아 사용할 수가 없다(컴파일 에러)

---
+++ src/com/godOfJava1/c13/InterfaceExample.java
@@ -0,0 +1,9 @@
+package com.godOfJava1.c13;
+
+import com.godOfJava1.c13.service.*;
+
+public class InterfaceExample {
+    public static void main(String[] args) {
+        MemberManager member = new MemberManagerImpl();
+    }
+}
