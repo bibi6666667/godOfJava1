2d342f40f210c69b526ae581daaf08b30dadbff1
bibi6666667 <bibi6666667@kakao.com>
2020-12-16 20:17:06 +0900

    [13장] 인터페이스 구현 - implements
    314-316
    
    -implements : 자바의 예약어로, 클래스명 뒤에 implements 인터페이스 를 추가하면 그 클래스에서 구현해야 하는 인터페이스를 정의할 수 있다. (만들어져 있는 인터페이스를 적용한다)
    - 인터페이스를 구현하는 것은 상속이 아니므로 여러 개를 implements할 수 있다.
    - 인터페이스를 구현할 경우 그 클래스 내에는 반드시 인터페이스에 정의된 메소드들의 몸통을 만들어 주어야만 한다.

---
+++ src/com/godOfJava1/c13/service/MemberManagerImpl.java
@@ -0,0 +1,21 @@
+package com.godOfJava1.c13.service;
+
+import com.godOfJava1.c13.model.MemberDTO;
+
+public class MemberManagerImpl implements MemberManager {
+    @Override
+    public boolean addMember(MemberDTO member) {
+        return false;
+    }
+
+    @Override
+    public boolean removeMember(String name, String phone) {
+        return false;
+    }
+
+    @Override
+    public boolean updateMember(MemberDTO member) {
+        return false;
+    }
+
+}
