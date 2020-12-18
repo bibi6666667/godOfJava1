b868ccfa2a5a2dde5ac06b11e73620175c3fcd35
bibi6666667 <bibi6666667@kakao.com>
2020-12-18 16:01:21 +0900

    [13장] enum클래스란
    327-328
    
    - enum클래스는 상수의 집합이다.
    - 타입이나 값을 지정할 필요 없이, 상수의 이름만 콤마로 구분해 나열한다.

---
+++ src/com/godOfJava1/c13/enums/OverTimeValues.java
@@ -0,0 +1,10 @@
+package com.godOfJava1.c13.enums;
+
+public enum OverTimeValues {
+    // enum클래스는 상수의 집합이다.
+    // 타입이나 값을 지정할 필요 없이, 상수의 이름만 콤마로 구분해 나열한다.
+    THREE_HOUR,
+    FIVE_HOUR,
+    WEEKEND_FOUR_HOUR,
+    WEEKEND_EIGHT_HOUR;
+}
