b40c681f63a607ffb93095fe469d28d2ac3eccb0
bibi6666667 <bibi6666667@kakao.com>
2020-12-19 22:51:55 +0900

    [13장] 직접 해 봅시다 (1) - 인터페이스와 abstract클래스
    336
    
    배열과 같이 목록을 관리하는 클래스를 위한 인터페이스를 만들고 구현하기.
    인터페이스, abstract클래스, abstract메소드 실습

---
+++ src/com/godOfJava1/c13/impl/list/AbstractList.java
@@ -0,0 +1,14 @@
+package com.godOfJava1.c13.impl.list;
+
+public abstract class AbstractList implements List {
+    public void add(Object value) {
+    }
+
+    public void update(int index, Object value){
+    }
+
+    public void remove(int index){
+    }
+
+    public abstract void clear();
+}
