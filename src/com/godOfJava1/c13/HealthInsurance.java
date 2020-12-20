d861dd43aca673a9474ebb9dc5f6f542e0345bb6
bibi6666667 <bibi6666667@kakao.com>
2020-12-20 22:51:30 +0900

    [13장] 직접 해 봅시다 (2) - 완료 (정답 참고)
    337-338 직접 해 봅시다(2) 문제 실습
    
    https://github.com/godofjava/GodOfJava2nd/blob/master/Chapter13/src/c/enums/HealthInsurance.java 의 정답 참고.
    - 4번 문제,
    - 5번 문제 에서 막혀서 답안을 보며 했다.

--- src/com/godOfJava1/c13/HealthInsurance.java
+++ src/com/godOfJava1/c13/HealthInsurance.java
@@ -1,12 +1,12 @@
 package com.godOfJava1.c13;
 
 public enum HealthInsurance {
-    LEVEL_ONE(1000, 0.01),
-    LEVEL_TWO(2000, 0.02),
-    LEVEL_THREE(3000, 0.032),
-    LEVEL_FOUR(4000, 0.045),
-    LEVEL_FIVE(5000, 0.056),
-    LEVEL_SIX(6000, 0.071);
+    LEVEL_ONE(1000, 1.0),
+    LEVEL_TWO(2000, 2.0),
+    LEVEL_THREE(3000, 3.2),
+    LEVEL_FOUR(4000, 4.5),
+    LEVEL_FIVE(5000, 5.6),
+    LEVEL_SIX(6000, 7.1);
     private final int maxSalary;
     private final double ratio;
 
@@ -15,25 +15,37 @@ public enum HealthInsurance {
         this.ratio = ratio;
     }
 
-    double getRatio(int maxSalary) { // 공제비율을 리턴하는 메소드
-        double returnRatio = 0.0;
-        HealthInsurance[] healthInsurancesList = HealthInsurance.values();
-        for (HealthInsurance one : healthInsurancesList) {
-            if (maxSalary == one.maxSalary) {
-                returnRatio = one.ratio;
-                break;
-            }
-        }
-        return returnRatio;
+    // 4. 공제비율을 리턴하는 메소드 만들기 (정답 참고함)
+    public double getRatio() { // 공제비율을 리턴하는 메소드
+        return ratio;
     }
 
+    // 5. 연봉을 매개변수로 받으면 그 연봉에 해당하는 enum객체 리턴하기 (정답 참고함)
     public static HealthInsurance getHealthInsurance(int salary) {
-        HealthInsurance[] healthInsurancesList = HealthInsurance.values();
-        for (HealthInsurance one : healthInsurancesList) {
-            if (salary == one.maxSalary) {
-                returnRatio = one.ratio;
-                break;
-            }
+        if (salary < 1000) {
+            return LEVEL_ONE;
+        } else if (salary < 2000) {
+            return LEVEL_TWO;
+        } else if (salary < 3000) {
+            return LEVEL_THREE;
+        } else if (salary < 4000) {
+            return LEVEL_FOUR;
+        } else if (salary < 5000) {
+            return LEVEL_FIVE;
+        } else {
+            return LEVEL_SIX;
         }
+    }
+
+    public static void main(String[] args) {
+        int[] salaryArray = new int[] {1500, 5500, 8000};
+        HealthInsurance[] insurances = new HealthInsurance[3];
+        insurances[0] = HealthInsurance.getHealthInsurance(salaryArray[0]);
+        insurances[1] = HealthInsurance.getHealthInsurance(salaryArray[1]);
+        insurances[2] = HealthInsurance.getHealthInsurance(salaryArray[2]);
+        for (int i = 0; i < 3; i++) {
+            System.out.println(salaryArray[i] + " = " + insurances[i] + " , " + insurances[i].getRatio());
+        }
+
     }
 }
