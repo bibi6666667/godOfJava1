9737b2673c9167a83683f87b810f4b7caf89e1de
bibi6666667 <bibi6666667@kakao.com>
2020-12-24 14:19:29 +0900

    [16장] 하나의 java파일에 여러 클래스 선언하기
    416
    
     여러 클래스를 하나의 java파일에 선언할 수 있다.
    이 java파일의 이름은 반드시 public한 클래스의 이름을 따서 만들어야 한다. (PublicClass.java)
    이때 JustNotPublicClass는  public한 클래스가 아닐 뿐, 내부클래스에 해당하지 않는다.

---
+++ src/com/godOfJava1/c16/PublicClass.java
@@ -0,0 +1,10 @@
+package com.godOfJava1.c16;
+
+// 여러 클래스를 하나의 java파일에 선언할 수 있다.
+// 이 java파일의 이름은 반드시 public한 클래스의 이름을 따서 만들어야 한다. (PublicClass.java)
+public class PublicClass {
+}
+
+class JustNotPublicClass {
+    // 이 클래스는 public한 클래스가 아닐 뿐, 내부클래스에 해당하지 않는다.
+}
