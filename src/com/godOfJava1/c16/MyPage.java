de5df2e37a11cf3c76fdbada6831afc2713ab22f
bibi6666667 <bibi6666667@kakao.com>
2020-12-25 15:50:53 +0900

    [16장] 직접 해 봅시다 - 정답 및 피드백
    427-428
    
    https://github.com/godofjava/GodOfJava2nd/blob/master/Chapter16/src/c/inner/practice/MyPage.java 의 정답을 보고 고친 코드.
    
    - 2. 클래스 변수이지만 static은 없었다
    - 7. InputBox 클래스에 선언된 listenerCalled() 메소드를 호출하여 onKeyDown()이벤트와 onKeyUp()이벤트가 수행되도록 하자.
     : MyPage의 인스턴스 변수 input의 메소드를 호출하고, 매개변수에서 InputBox클래스의 상수 KEY_DOWN과 KEY_UP을 호출하는 방식으로 구현해야 한다.

--- src/com/godOfJava1/c16/MyPage.java
+++ src/com/godOfJava1/c16/MyPage.java
@@ -1,9 +1,7 @@
 package com.godOfJava1.c16;
 
-import java.security.Key;
-
 public class MyPage {
-    static InputBox input;
+    InputBox input;
 
     public static void main(String[] args) {
         MyPage myPage = new MyPage();
@@ -11,13 +9,13 @@ public class MyPage {
         myPage.pressKey();
     }
 
-    public void setUI(){
+    public void setUI() {
         input = new InputBox(); // 참조타입 만들떄는 new필요..
         KeyEventListener listener = new KeyEventListener() { // KeyEventListener를 구현한 익명 클래스
             public void onKeyDown() {
                 System.out.println("Key Down");
             }
-            
+
             public void onKeyUp() {
                 System.out.println("Key Up");
             }
@@ -26,8 +24,11 @@ public class MyPage {
     }
 
     public void pressKey() {
-        InputBox inputBox = new InputBox();
-        inputBox.listenerCalled(4);
-        inputBox.listenerCalled(2);
+        //  7. InputBox 클래스에 선언된 listenerCalled() 메소드를 호출하여 onKeyDown()이벤트와 onKeyUp()이벤트가 수행되도록 하자.
+//      InputBox inputBox = new InputBox();
+//      inputBox.listenerCalled(2);
+//      inputBox.listenerCalled(4); 이렇게 호출하는 것이 아닌..
+        input.listenerCalled(InputBox.KEY_DOWN);
+        input.listenerCalled(InputBox.KEY_UP); // 이렇게 해야 한다.
     }
 }
