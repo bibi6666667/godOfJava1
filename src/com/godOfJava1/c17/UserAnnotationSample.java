558f37209de7b4f3a92f2a9c892510b96f0bb47d
bibi6666667 <bibi6666667@kakao.com>
2020-12-27 00:01:16 +0900

    [17장] 어노테이션 볌위 설정
    440-441
    
    - 어노테이션은 클래스에도 지정할 수 있으나, @UserAnnotation 어노테이션은 범위가 메소드까지라 에러가 발생함.
    - 클래스에서도 사용할 수 있게 하려면 @UserAnnotation 선언에서 @Target 범위를 넓혀야 한다. (UserAnnotation.java 9번 줄 참고)

--- src/com/godOfJava1/c17/UserAnnotationSample.java
+++ src/com/godOfJava1/c17/UserAnnotationSample.java
@@ -1,5 +1,7 @@
 package com.godOfJava1.c17;
 
+// @UserAnnotation(number = 0) : 어노테이션은 클래스에도 지정할 수 있으나, @UserAnnotation 어노테이션은 범위가 메소드까지라 에러가 발생함.
+// 클래스에서도 사용할 수 있게 하려면 @UserAnnotation 선언에서 @Target 범위를 넓혀야 한다. (UserAnnotation.java 9번 줄 참고)
 public class UserAnnotationSample {
     // 아래처럼 메소드를 선언할 때, UserAnnotation에서 직접 만든 어노테이션을 사용할 수 있다.
     // 어노테이션 선언 클래스의 각 메소드 이름에 해당하는 값을 소괄호 안에 넣어 주어야 한다.
