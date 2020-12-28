24899fb714440dfc6da2fc375c61b1e690a83337
bibi6666667 <bibi6666667@kakao.com>
2020-12-28 22:59:34 +0900

    [18장] 메소드의 선언
    463
    
    - 메소드 method : 자바에서 메소드는 클래스의 행위 behavior를 처리하는 데 사용됨
     public : 접근제어자 - public
     void : 리턴타입 - void (리턴값없음)
     method : 메소드명
     소괄호 : 매개변수 parameter 의 위치. String타입의 param 매개변수와 int타입의 params 매개변수를 갖는 메서드.

---
+++ src/com/godOfJava1/c18/Method.java
@@ -0,0 +1,11 @@
+package com.godOfJava1.c18;
+
+public class Method {
+    public void method(String param, int params) {
+        // 메소드 method : 자바에서 메소드는 클래스의 행위 behavior를 처리하는 데 사용됨
+        // public : 접근제어자 - public
+        // void : 리턴타입 - void (리턴값없음)
+        // method : 메소드명
+        // 소괄호 : 매개변수 parameter 의 위치. String타입의 param 매개변수와 int타입의 params 매개변수를 갖는 메서드.
+    }
+}
