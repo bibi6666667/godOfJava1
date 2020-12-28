db13304115a17cd0f7fba6c01c88c378197c6c51
bibi6666667 <bibi6666667@kakao.com>
2020-12-28 16:13:20 +0900

    [18장] 클래스 선언 예시
    460-461
    
    - 클래스 선언 : Sample이라는 이름의 클래스 선언
    - 클래스 확장 : extends로 Parent라는 이름의 부모클래스 확장. 하나만 지정 가능. abstract클래스도 가능.
     - 인터페이스 구현 : implements 뒤에 구현할 인터페이스 이름을 나열. 한 개 이상 구현 가능. 해당 인터페이스의 모든 메소드를 구현해야 함.

---
+++ src/com/godOfJava1/c18/Sample.java
@@ -0,0 +1,9 @@
+package com.godOfJava1.c18;
+
+import com.godOfJava1.c13.InterfaceExample;
+
+public class Sample extends Parent /*implements Interface1, Interface2*/ {
+    // 클래스 선언 : Sample이라는 이름의 클래스 선언
+    // 클래스 확장 : extends로 Parent라는 이름의 부모클래스 확장. 하나만 지정 가능. abstract클래스도 가능.
+    // 인터페이스 구현 : implements 뒤에 구현할 인터페이스 이름을 나열. 한 개 이상 구현 가능. 해당 인터페이스의 모든 메소드를 구현해야 함.
+}
