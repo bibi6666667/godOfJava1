11adaab97dc3e3c9efa639aea24a49d34f70e802
bibi6666667 <bibi6666667@kakao.com>
2020-12-28 16:25:02 +0900

    [18장] enum 클래스 선언 예
    462
    
    enum클래스 : enum으로 선언하며, 상수를 열거하기 위한 용도로 사용된다.
     상수의 이름만 정의해도 되고, enum클래스 내에 생성자를 만들어 상수값을 지정할 수도 있다.
     enum클래스 내에 메소드를 만들어 기능을 추가할 수도 있다.

---
+++ src/com/godOfJava1/c18/EnumClass.java
@@ -0,0 +1,9 @@
+package com.godOfJava1.c18;
+
+public enum EnumClass {
+    THREE_HOUR/*(18000)*/,
+    FIVE_HOUR/*(30000)*/
+    // enum클래스 : enum으로 선언하며, 상수를 열거하기 위한 용도로 사용된다.
+    // 상수의 이름만 정의해도 되고, enum클래스 내에 생성자를 만들어 상수값을 지정할 수도 있다.
+    // enum클래스 내에 메소드를 만들어 기능을 추가할 수도 있다.
+}
