ba259d78238b84316af0886a51c2a0adc8e613fb
bibi6666667 <bibi6666667@kakao.com>
2020-12-28 16:24:34 +0900

    [18장] abstract 클래스 선언 예
    462
    
    abstract 클래스 선언 : abstract class로 선언되어야 하며, abstract로 선언된 메소드가 하나라도 있으면 absctact class이다.

---
+++ src/com/godOfJava1/c18/AbstractClass.java
@@ -0,0 +1,8 @@
+package com.godOfJava1.c18;
+
+public abstract class AbstractClass {
+    public abstract void methodC();
+    public void methodD() {
+    }
+    // abstract 클래스 선언 : abstract class로 선언되어야 하며, abstract로 선언된 메소드가 하나라도 있으면 absctact class이다.
+}
