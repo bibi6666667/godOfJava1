0dc48021b680b6e65a3433af5f7229861022de6a
bibi6666667 <bibi6666667@kakao.com>
2020-12-28 15:10:41 +0900

    [18장] 정리 - 패키지와 import
    452
    
    - 패키지 package : 클래스들을 그룹화하기 위한 단위.
     반드시 소스코드의 가장 첫 줄에 존재해야 함.
     패키지명은 모두 소문자로 하는 것이 좋으며, java나 javax로 시작하거나 자바 예약어가 포함되면 안 된다.
    - import : 다른 패키지에 있는 클래스를 사용하기 위한 문장.
     다른 클래스의 접근 가능한 static변수를 참조하려면 import static을 사용한다.
     패키지 내의 모든 클래스를 참조하려면 * 를 사용한다.

---
+++ src/com/godOfJava1/c18/Package.java
@@ -0,0 +1,12 @@
+package com.godOfJava1.c18;
+// 패키지 package : 클래스들을 그룹화하기 위한 단위.
+// 반드시 소스코드의 가장 첫 줄에 존재해야 함.
+// 패키지명은 모두 소문자로 하는 것이 좋으며, java나 javax로 시작하거나 자바 예약어가 포함되면 안 된다.
+
+import com.godOfJava1.c1_3.*;
+// import : 다른 패키지에 있는 클래스를 사용하기 위한 문장.
+// 다른 클래스의 접근 가능한 static변수를 참조하려면 import static을 사용한다.
+// 패키지 내의 모든 클래스를 참조하려면 * 를 사용한다.
+
+public class Package {
+}
